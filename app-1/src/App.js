c93932dc319f8dd729b843523a23e942d8e89c09
Ethan Bird <eth51473@gmail.com>
2021-10-08 09:26:46 -0600

    finished app-1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [msg, setMessage] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <input type="text" onChange ={(event) => setMessage(event.target.value)}></input>
+      <h2>{msg}</h2>
     </div>
   );
 }
