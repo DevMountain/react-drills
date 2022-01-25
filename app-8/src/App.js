46cdab4d97b3856bf56e06d958c4f4add7d74a30
Freeman Bird <freemanbird@gmail.com>
2022-01-25 08:10:49 -0700

    completed app-8

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component{
   constructor(){
     super();
     this.state = {
+      verse: '',
+      
     }
   }
-
+  componentDidMount() {
+    axios.get('https://bible-api.com/john 3:16?translation=kjv').then(res => {
+      this.setState({verse: res.data});
+    })
+  }
   render() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <p>{this.state.verse.reference}</p>
+      <p>{this.state.verse.text}</p>
     </div>
   );
 }
